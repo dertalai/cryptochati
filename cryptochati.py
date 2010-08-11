ba77ce2fe82fcba27b718d2572b6f2a04e6a7fc2
Dertalai <dertalai@gmail.com>
2010-08-12 00:53:05 +0200

    First files

---
+++ cryptochati.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+############################################################################
+##                                                                        ##
+## Cryptochati XChat-plugin                                               ##
+##                                                                        ##
+## Copyright (C) 2010 Dertalai <base64:'ZGVydGFsYWlAZ21haWwuY29t'>        ##
+##                                                                        ##
+## This program is free software: you can redistribute it and/or modify   ##
+## it under the terms of the GNU General Public License as published by   ##
+## the Free Software Foundation, either version 3 of the License, or      ##
+## (at your option) any later version.                                    ##
+##                                                                        ##
+## This program is distributed in the hope that it will be useful,        ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of         ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          ##
+## GNU General Public License for more details.                           ##
+##                                                                        ##
+## You should have received a copy of the GNU General Public License      ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.  ##
+##                                                                        ##
+############################################################################
+
+"""Cryptochati XChat-plugin
+
+TODO
+
+Installation:
+    Copy the file named "cryptochati.py" in the "~/.xchat2/" directory
+
+Running:
+    The plugin should be autoloaded the next time Xchat start. But you can
+	launch it manually with the following command (in Xchat):
+	
+	/py load cryptochati.py
+
+Xchat commands:
+    TODO
+"""
+
+__version__ = "0"
+__author__ = "Dertalai <base64:'ZGVydGFsYWlAZ21haWwuY29t'>"
+__copyright__ = \
+    "Copyright © 2010 Dertalai <base64:'ZGVydGFsYWlAZ21haWwuY29t'>"
+
+__module_name__ = "Cryptochati XChat-plugin"
+__module_version__ = __version__
+__module_description__ = __doc__
+__module_author__ = __author__
+
+
